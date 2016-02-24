e6b02dcf51035deaa618aee14e9f3e543ac4570a
Joao Rodrigues <JoaoRodrigues@Joaos-Air-3.lan>
2016-02-24 19:49:43 +0000

    estruturacao de codigo e adicionados alguns comments

--- src/client/MyWhats.java
+++ src/client/MyWhats.java
@@ -13,20 +13,37 @@ public class MyWhats {
 
 	public static void main(String[]args){
 		
-		//criar uma ligação basica
+		//validacao de input
+		//formato <localUser> <ip:port> -p <password> -m <contact> <body>
+		//possiveis flags -m, -f, -r, -a, -d
+		//minimum length 3 <localUser> <ip:port> -r
 		
 		try{
+			
+			//User
 			UserBuilder userFactory = new UserBuilder();
 			User user = userFactory.make(args);
-
+			
+			//Estabelece ligacao
 			Connection connection = new Connection(new Socket("127.0.0.1", 8080));
+			
+			//Formula request
 			Request request = new Request();
 			request.setType("-m");
 			request.setMessage(new Message("Joao", "Simao", "Hey you! :P"));
 			request.setUser(user);
+			
+			//Envia request
 			connection.getOutputStream().writeObject(request);
+			
+			//Obtem reply
 			Reply reply = (Reply) connection.getInputStream().readObject();
-			System.out.println(reply.getStatus());
+			
+			//Se erro
+			if(reply.getStatus() != 200)
+				System.out.println(reply.getMessage());
+			
+			//Fecha connection
 			connection.destroy();
 			
 		}catch(Exception e){
