f3263f65be62a56a3ee7275f263eea91c58f633b
Joao Rodrigues <JoaoRodrigues@Joaos-MacBook-Air-3.local>
2016-02-26 14:46:55 +0000

    Adicionada a funcao no servidor para garantir a existencia de todas as pastas necessárias para a base dados

--- src/server/MyWhatsServer.java
+++ src/server/MyWhatsServer.java
@@ -1,5 +1,6 @@
 package server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,6 +10,9 @@ import java.net.Socket;
 public class MyWhatsServer {
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		//init fs
+		prepareFileStructure();
 		
 		@SuppressWarnings("resource")
 		ServerSocket server = new ServerSocket(8080);
@@ -26,4 +30,75 @@ public class MyWhatsServer {
 
 	}
 
+	//Metodos utilizados para garantir a existencia
+	//de todos os directorios necessarios para
+	//a execucao do programa
+	private static void prepareFileStructure(){
+		makeDatabase();
+		makeDatabaseUsers();
+		makeDatabaseGroups();
+		makeDatabaseConversations();
+		makeDatabaseMessages();
+	}
+	private static void makeDatabaseConversations() {
+		try{
+			File file = new File("DATABASE/CONVERSATIONS");
+			if(!file.isDirectory())
+				if(!file.mkdir()){
+					new Exception();
+				}
+		}catch(Exception e){
+			System.out.println("Erro ao criar estrutura de pastas inicial");
+			System.exit(-1);
+		}
+	}
+	private static void makeDatabaseMessages() {
+		try{
+			File file = new File("DATABASE/MESSAGES");
+			if(!file.isDirectory())
+				if(!file.mkdir()){
+					new Exception();
+				}
+		}catch(Exception e){
+			System.out.println("Erro ao criar estrutura de pastas inicial");
+			System.exit(-1);
+		}
+	}
+	private static void makeDatabaseGroups() {
+		try{
+			File file = new File("DATABASE/GROUPS");
+			if(!file.isDirectory())
+				if(!file.mkdir()){
+					new Exception();
+				}
+		}catch(Exception e){
+			System.out.println("Erro ao criar estrutura de pastas inicial");
+			System.exit(-1);
+		}
+	}
+	private static void makeDatabaseUsers() {
+		try{
+			File file = new File("DATABASE/USERS");
+			if(!file.isDirectory())
+				if(!file.mkdir()){
+					new Exception();
+				}
+		}catch(Exception e){
+			System.out.println("Erro ao criar estrutura de pastas inicial");
+			System.exit(-1);
+		}
+	}
+	private static void makeDatabase() {
+		try{
+			File file = new File("DATABASE");
+			if(!file.isDirectory())
+				if(!file.mkdir()){
+					new Exception();
+				}
+		}catch(Exception e){
+			System.out.println("Erro ao criar estrutura de pastas inicial");
+			System.exit(-1);
+		}
+	}
+	
 }
