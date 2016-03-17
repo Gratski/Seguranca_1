6731b897186cccffd98fce9f2be1324836df2d00
Simão Neves <simaocostaneves@gmail.com>
2016-03-17 09:20:53 +0000

    Changed server and client policies, refactored proxy paths, fixed bug in -r

--- src/handlers/RequestHandler.java
+++ src/handlers/RequestHandler.java
@@ -182,6 +182,7 @@ public class RequestHandler extends Thread {
 			reply.setMessage("Não existem nenhumas conversas");
 		} else {
 			reply.setStatus(200);
+			reply.setType("all");
 			reply.setConversations(conversations);
 		}
 		return reply;
@@ -196,6 +197,7 @@ public class RequestHandler extends Thread {
 			reply.setMessage("Não existem conversas entre " + req.getUser().getName() + " e " + req.getContact());
 		} else {
 			reply.setStatus(200);
+			reply.setType("single");
 			reply.addConversation(conversation);
 		}
 		return reply;
--- src/proxies/Proxy.java
+++ src/proxies/Proxy.java
@@ -1,16 +1,16 @@
 package proxies;
 
 public class Proxy {
-	protected static final String DATABASE = "DATABASE";
+	protected static final String DATABASE = "DATABASE/";
 	
 	protected static final String USERS_INDEX = DATABASE + "/USERS";
 	protected static final String GROUPS_INDEX = DATABASE + "/GROUPS";
 	
-	protected static final String CONVERSATIONS = DATABASE + "/CONVERSATIONS";
-	protected static final String CONVERSATIONS_PRIVATE_INDEX = DATABASE + "/"+CONVERSATIONS + "/INDEX";
-	protected static final String CONVERSATIONS_PRIVATE = DATABASE + "/" + CONVERSATIONS + "/PRIVATE";
-	protected static final String CONVERSATIONS_GROUP = DATABASE + "/" + CONVERSATIONS + "/GROUP";
+	protected static final String CONVERSATIONS = DATABASE + "CONVERSATIONS/";
+	protected static final String CONVERSATIONS_PRIVATE_INDEX = CONVERSATIONS + "INDEX";
+	protected static final String CONVERSATIONS_PRIVATE = CONVERSATIONS + "PRIVATE/";
+	protected static final String CONVERSATIONS_GROUP = CONVERSATIONS + "GROUP/";
 
-	protected static final String FILES_FOLDER = "/FILES";
+	protected static final String FILES_FOLDER = "FILES/";
 	protected static final String MESSAGE_FILE_EXTENSION = ".msg";
 }
